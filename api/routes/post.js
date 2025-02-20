5d8f50f5333ec4ad6445fcdd936be71026d1bf40
Guilherme <111528193+guihi1@users.noreply.github.com>
2025-02-20 11:32:53 -0300

    Require authentication for routes that modify a post

--- api/routes/post.js
+++ api/routes/post.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import * as postController from '../controllers/postController.js';
+import * as auth from '../utils/auth.js';
 
 const router = Router();
 
@@ -7,10 +8,25 @@ router.get('/', postController.getPosts);
 
 router.get('/:postId', postController.getPostById);
 
-router.post('/', postController.createPost);
+router.post(
+	'/',
+	auth.authenticateUser,
+	auth.authorizeRoles(['AUTHOR', 'ADMIN']),
+	postController.createPost,
+);
 
-router.put('/:postId', postController.updatePost);
+router.put(
+	'/:postId',
+	auth.authenticateUser,
+	auth.authorizeRoles(['AUTHOR', 'ADMIN']),
+	postController.updatePost,
+);
 
-router.delete('/:postId', postController.deletePost);
+router.delete(
+	'/:postId',
+	auth.authenticateUser,
+	auth.authorizeRoles(['AUTHOR', 'ADMIN']),
+	postController.deletePost,
+);
 
 export default router;
