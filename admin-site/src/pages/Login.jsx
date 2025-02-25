74754570c87040b42b686960235a7661a40834eb
Guilherme <111528193+guihi1@users.noreply.github.com>
2025-02-25 10:32:51 -0300

    Add functions to handle the login

--- admin-site/src/pages/Login.jsx
+++ admin-site/src/pages/Login.jsx
@@ -1,9 +1,49 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const Login = () => {
+	const [formData, setFormData] = useState({ email: '', password: '' });
+	const [error, setError] = useState('');
+	const navigate = useNavigate();
+
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setError('');
+		if (!formData.email || !formData.password) {
+			setError('Please fill in all fields');
+			return;
+		}
+
+		try {
+			const response = await fetch('http://localhost:3000/login', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					email: formData.email,
+					password: formData.password,
+				}),
+			});
+
+			const data = await response.json();
+
+			if (!response.ok) throw new Error(data.message || 'Login failed!');
+			localStorage.setItem('token', data.token);
+			setFormData({ email: '', password: '' });
+			setTimeout(() => navigate('/dashboard'), 1500);
+		} catch (err) {
+			setError(err.message);
+		}
+	};
+
 	return (
 		<div className="flex items-center justify-center min-h-screen bg-base-100 px-4">
 			<div className="w-full sm:w-96 md:w-[32rem] lg:w-[36rem] bg-base-200 p-6 rounded-lg shadow-lg">
 				<h1 className="text-2xl font-bold text-center">Sign In</h1>
-				<form className="flex flex-col gap-4 m-4">
+				<form className="flex flex-col gap-4 m-4" onSubmit={handleSubmit}>
 					<label className="floating-label">
 						<span>Email</span>
 						<input
@@ -12,6 +52,8 @@ const Login = () => {
 							type="email"
 							id="email"
 							name="email"
+							onChange={handleChange}
+							value={formData.email}
 						/>
 					</label>
 					<label className="floating-label">
@@ -22,8 +64,12 @@ const Login = () => {
 							type="password"
 							id="password"
 							name="password"
+							onChange={handleChange}
+							value={formData.password}
 						/>
 					</label>
+					{error && <p className="text-error">{error}</p>}
+
 					<button className="btn w-full bg-primary text-xl" type="submit">
 						Login
 					</button>
