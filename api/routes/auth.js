be410032c8d9681cce3296c746f3245c587d67dc
Guilherme <111528193+guihi1@users.noreply.github.com>
2025-02-26 11:20:09 -0300

    Add route to verify if a token is valid

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,23 +1,28 @@
 import { Router } from 'express';
 import { body, validationResult } from 'express-validator';
 import * as userController from '../controllers/userController.js';
+import * as auth from '../utils/auth.js';
 
 const router = Router();
 
 router.post(
-  '/login',
-  [
-    body('email').trim().isEmail().withMessage('Enter a valid email'),
-    body('password').exists().withMessage('Password is required'),
-  ],
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ message: errors.array() });
-    }
-    next();
-  },
-  userController.loginUser,
+	'/login',
+	[
+		body('email').trim().isEmail().withMessage('Enter a valid email'),
+		body('password').exists().withMessage('Password is required'),
+	],
+	(req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ message: errors.array() });
+		}
+		next();
+	},
+	userController.loginUser,
 );
+
+router.get('/verify-token', auth.authenticateUser, (req, res) => {
+	res.json({ valid: true, user: req.user });
+});
 
 export default router;
